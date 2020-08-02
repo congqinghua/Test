d5928a1ae130190747e50ac5f8d909fa59e4b32c
Username@example.com <hp@cong>
2020-08-02 09:59:29 +0800

    number 1.0

--- JavaBasic9/src/com/test1/A.java
+++ JavaBasic9/src/com/test1/A.java
@@ -1,9 +1,10 @@
 package com.test1;
 
-public class Test {
+public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("Hello world");
 	}
 
